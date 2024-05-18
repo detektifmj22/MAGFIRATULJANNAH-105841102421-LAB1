21656c4527d47c89a404f170017f5c84d0343c24
detektifmj22 <105841102421@student.unismuh.ac.id>
2024-05-18 20:23:59 +0800

    tugas1

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-import { View } from 'react-native';
 export default function App() {
   return (
-    <View style={{flex:1,}}>
-      <View style={{flex:1, flexDirection:'row'}}>
-      <View style={{flex:1, flexDirection:'column'}}>
-        <View style={{flex:1, backgroundColor:'white',}}></View>
-        <View style={{flex:1, backgroundColor:'black',}}></View>
+    <View style={{flex: 1,}}>
+        <View style={{flex: 1, flexDirection: 'row' }}>
+            <View style={{flex: 1,flexDirection: 'column'}}>
+              <View style={{flex: 1,backgroundColor: 'white',}}></View>
+              <View style={{flex: 1,backgroundColor: 'green',}}></View>
+            </View>
+            <View style={{flex: 1,flexDirection: 'column'}}>
+                  <View style={{flex: 1,flexDirection: 'row'}}>
+                    <View style={{flex: 1,backgroundColor: 'blue',}}></View>
+                    <View style={{flex: 1,backgroundColor: 'red',}}></View>
+                  </View>
+            </View>
+        </View>
+      <View style={{flex: 1,backgroundColor: 'green',}}></View>
     </View>
-
-    <View style={{flex:1, flexDirection:'column'}}>
-    <View style={{flex:1, flexDirection:'row'}}> 
-        <View style={{flex:1, backgroundColor:'blue',}}></View>
-        <View style={{flex:1, backgroundColor:'red',}}></View>
-    </View>
-    </View>
-    </View>
-    <View style={{flex:1, backgroundColor:'green',}}></View>
-  </View>
   );
 }
