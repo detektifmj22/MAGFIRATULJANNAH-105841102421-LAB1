df680a743bcb37d2bd8f5e2292ea6281b34ef165
detektifmj22 <105841102421@student.unismuh.ac.id>
2024-05-18 12:03:03 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import { View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{flex: 1,
+      flexDirection:'column'}}>
+    <View style={{
+      flex: 1,backgroundColor:'red'}}> 
+    </View>
+    <View style={{flex: 1,backgroundColor:'green'}}> 
+    </View>
+    <View style={{flex: 1,backgroundColor:'blue'}}> 
+    </View>
+  </View>
+  );
+  }
