5680154a906abda30f8f40a20e07dae707d1a26f
detektifmj22 <105841102421@student.unismuh.ac.id>
2024-05-18 20:51:15 +0800

    tugas1

---
+++ App.js
@@ -0,0 +1,20 @@
+export default function App(){
+    return (
+        <View style={{flex: 1,}}>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{flex: 1, flexDirection: 'column'}}>
+            <View style={{flex: 1,backgroundColor: 'white'}}></View>
+            <View style={{flex: 1,backgroundColor: 'black'}}></View>
+        </View>
+
+        <View style={{flex: 1, flexDirection: 'column'}}>
+        <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{flex: 1,backgroundColor: 'blue'}}></View>
+            <View style={{flex: 1,backgroundColor: 'red'}}></View>
+        </View>
+        </View>
+        </View>
+            <View style={{flex: 1,backgroundColor: 'green'}}></View>
+        </View>
+    );
+}
