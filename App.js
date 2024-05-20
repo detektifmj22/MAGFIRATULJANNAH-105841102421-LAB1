3b221fbfcd8c8cfa356139107d1b1acb5445380d
detektifmj22 <105841102421@student.unismuh.ac.id>
2024-05-20 14:42:49 +0800

    percobaan ke-2

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+
 export default function App() {
     return (
-        <View style={{flex: 1,}}>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-        <View style={{flex: 1, flexDirection: 'column'}}>
-            <View style={{flex: 1,backgroundColor: 'white'}}></View>
-            <View style={{flex: 1,backgroundColor: 'black'}}></View>
+        <View style = {{
+            flex:1,
+            backgroundColor: 'white',
+            justifyContent:'center', //kolom
+            alignItems:'flex-end', //baris
+            flexDirection:'row',
+            marginBottom: 100,
+        }}>
+            <View style = {{
+            backgroundColor: 'red',
+            width: 150,
+            height: 70,
+            borderRadius: 10,
+            marginRight:10
+        }}>
+            <Text style={{
+                color: 'white',
+                textAlign: 'center',
+                lineHeight: 80,
+                fontSize: 30,
+                fontWeight: 'bold',
+        }}>
+            Login
+        </Text>
         </View>
-
-        <View style={{flex: 1, flexDirection: 'column'}}>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-            <View style={{flex: 1,backgroundColor: 'blue'}}></View>
-            <View style={{flex: 1,backgroundColor: 'red'}}></View>
+            <View style = {{
+            backgroundColor: 'blue',
+            width: 150,
+            height: 70,
+            borderRadius: 10,
+            marginLeft:10
+        }}>
+            <Text style={{
+                color: 'white',
+                textAlign: 'center',
+                lineHeight: 70,
+                fontSize: 30,
+                fontWeight: 'bold',
+        }}>
+            Register
+        </Text>
         </View>
-        </View>
-        </View>
-            <View style={{flex: 1,backgroundColor: 'green'}}></View>
         </View>
     );
 }
