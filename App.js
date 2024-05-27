05dae356b2bdc6d4f21eeeecb172c34135130132
detektifmj22 <105841102421@student.unismuh.ac.id>
2024-05-27 14:51:00 +0800

    komponen

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
-import { View, Image, Button } from 'react-native';
-import ButtonCustom from './button';
-import TextCustom from './TextCustom';
-import ImagesT from './gambar';
-
-export default function App() {
+import { Text, View } from 'react-native';
+const  ButtonCustom = () => {
     return (
-        <>
-        <TextCustom />
-        <ImagesT />
-        <ButtonCustom />
-        </>
+        <View style = {{
+            flex:1,
+            backgroundColor: 'white',
+            justifyContent:'center', //kolom
+            alignItems:'flex-end', //baris
+            flexDirection:'row',
+            marginBottom: 100,
+        }}>
+            <ButtonComponent backgroundColor = 'red' text = 'Login'/>
+            <ButtonComponent backgroundColor = 'green' text = 'Register'/>
+            </View>
+        )
+    }
+
+    const ButtonComponent = ({backgroundColor, text}) => {
+        return (
+            <View style = {{
+                backgroundColor:backgroundColor,
+                width: 150,
+                height: 70,
+                borderRadius: 10,
+                marginRight:10
+        }}>
+            <Text style={{
+                color: 'white',
+                textAlign: 'center',
+                lineHeight: 80,
+                fontSize: 30,
+                fontWeight: 'bold',
+        }}>
+            {text}
+        </Text>
+        </View>
     );
 }
+
+export default ButtonCustom;
