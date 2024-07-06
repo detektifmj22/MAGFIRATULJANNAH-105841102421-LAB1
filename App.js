1db9b164ef0669563fded2db9781615160a76f2f
MAGFIRATUL JANNAH <138551164+detektifmj22@users.noreply.github.com>
2024-07-06 07:08:04 +0000

    UseHook Data & On Change

--- App.js
+++ App.js
@@ -1,44 +1,36 @@
-import React, { useState } from 'react'
-import { Text, TextInput, View } from 'react-native'
-import { Button } from 'react-native-web'
+import React, { useEffect, useState } from 'react'
+import { Text, View } from 'react-native'
 const App = () => {
-  const [formLogin, setForm] = useState({
-    email: '',
-    password: ''
-  })
+  const [users, setUsers] = useState([])
 
-  const onsubmit = () => {
-    if (formLogin.email === 'unismuh' 
-      && formLogin.password === 'unismuh') {
-      alert('Login Berhasil')
-    }
-
-  }
-
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => setUsers(json))
+  }, [])
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Form Login</Text>
-      <View>
-        <Text>Email</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={(hasil) => setForm({ ...formLogin, email: hasil })}
-          value={formLogin.email}
-        />
-        <Text>Password</Text>
-        <TextInput
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={(text) => setForm({ ...formLogin, password: text })}
-          value={formLogin.password}
-        />
-        <View style={{ marginTop: 10 }}>
-          <Button title="Login" onPress={onsubmit} />
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 10
+    }}>
+      <Text>Daftar User</Text>
+      {users.map((user) => (
+        <View
+          style={{
+            borderWidth: 1,
+            borderColor: 'black',
+            padding: 20,
+            margin: 5
+          }}
+          key={user.id}>
+          <Text>Nama: {user.name}</Text>
+          <Text>Username: {user.username}</Text>
+          <Text>Email: {user.email}</Text>
+          <Text>Street :{user?.address?.street}</Text>
         </View>
-        <View>
-          <Text>Email: {formLogin.email}</Text>
-          <Text>Password: {formLogin.password}</Text>
-        </View>
-      </View>
+      ))}
     </View>
   )
 }
